730e3c680e43bd43487c50e816b9b1e201b48eb3
sobirjon001 <sobirjon001@gmail.com>
2020-09-01 16:16:03 -0400

    new

--- src/App.js
+++ src/App.js
@@ -1,123 +1,116 @@
-import React, { useEffect, useState, useRef } from "react";
+import React from "react";
 import Recipe from "./Recipe";
 import "./App.css";
 
-const App = () => {
-  const APP_ID = "41456eaf";
-  const APP_KEY = "0c679c6c87d15f4a447a8c28afed63b2";
-  const searchInput = useRef(null);
-
-  const [recipes, setRecipes] = useState([]);
-  const [search, setSearch] = useState("");
-  const [query, setQuery] = useState("chicken");
-  const [recipe_recs, setRecip_recs] = useState([]);
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      recipes: [],
+      search: "",
+      query: "chicken",
+      recipe_recs: [],
+      APP_ID: "41456eaf",
+      APP_KEY: "0c679c6c87d15f4a447a8c28afed63b2",
+    };
+    this.loadStorage = this.loadStorage.bind(this);
+    this.getRecipes = this.getRecipes.bind(this);
+    this.updateSearch = this.updateSearch.bind(this);
+    this.getSearch = this.getSearch.bind(this);
+    this.saveToStorage = this.saveToStorage(this);
+  }
 
   // load storage
-  useEffect(() => {
-    loadStorage();
-    console.log(recipe_recs);
-  }, []);
-
-  useEffect(async () => {
-    getRecipes();
-    // const data = await { getRecipes };
-    // console.log("happend");
-    // console.log(data.hits);
-    // setRecipes(data.hits);
-    // setRecip_recs([...recipe_recs, { title: data.q, hits: data.hits }]);
-    saveToStorage();
-  }, [query]);
-
-  const loadStorage = () => {
+  loadStorage() {
     if (localStorage.getItem("recipe_recs") === null) {
       localStorage.setItem("recipe_recs", JSON.stringify([]));
     } else {
-      setRecip_recs(JSON.parse(localStorage.getItem("recipe_recs")));
+      this.setState({
+        recipe_recs: JSON.parse(localStorage.getItem("recipe_recs")),
+      });
     }
-  };
+  }
 
-  const saveToStorage = () => {
-    localStorage.setItem("recipe_recs", JSON.stringify(recipe_recs));
-  };
-
-  // const getRecipes = async () => {
-  const getRecipes = () => {
-    if (recipe_recs.includes((recipe_rec) => recipe_rec.title === query)) {
+  getRecipes() {
+    if (
+      this.state.recipe_recs.includes(
+        (recipe_rec) => recipe_rec.title === this.state.query
+      )
+    ) {
       alert("This recipe already saved. Please load it from saved recipes");
-      setSearch("");
-      searchInput.current.focus();
+      this.setState({ search: "" });
     } else if (
-      query === "" ||
-      query === " " ||
-      query === "  " ||
-      query === "   "
+      this.state.query === "" ||
+      this.state.query === " " ||
+      this.state.query === "  " ||
+      this.state.query === "   "
     ) {
       alert("Please wrigh valid recipe for cearch");
-      setSearch("");
-      searchInput.current.focus();
+      this.state.search("");
     } else {
-      // const responce = await fetch(
       fetch(
-        `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`
-        // );
+        `https://api.edamam.com/search?q=${this.state.query}&app_id=${this.state.APP_ID}&app_key=${this.state.APP_KEY}`
       )
         .then((rec) => rec.json())
         .then((data) => {
-          console.log("happend");
+          console.log("happend 1");
           console.log(data.hits);
-          setRecipes(data.hits);
+          this.setState({ recipes: [data.hits] });
+          console.log("happend 2");
           console.log(data.hits);
-          setRecip_recs([...recipe_recs, { title: data.q, hits: data.hits }]);
+          this.setState({
+            recipe_recs: [
+              ...this.state.recipe_recs,
+              { title: data.q, hits: [data.hits] },
+            ],
+          });
         });
-
-      // const data = await responce.json();
-      // return await data;
-      // console.log("happend");
-      // console.log(data.hits);
-      // console.log("happend 2");
-      // setRecip_recs([...recipe_recs, { title: data.q, hits: data.hits }]);
-
-      // console.log(recipe_recs);
-      // setRecipes(data.hits);
-      // saveToStorage();
+      this.saveToStorage();
     }
-  };
+  }
+  updateSearch(e) {
+    this.setState({ search: e.target.value });
+  }
 
-  const updateSearch = (e) => {
-    setSearch(e.target.value);
-  };
-
-  const getSearch = (e) => {
+  getSearch(e) {
     e.preventDafault();
-    setQuery(search);
-    setSearch("");
-  };
+    console.log(e);
+    this.setState({ query: this.state.search });
+    this.setState({ search: "" });
+    this.getRecipes();
+  }
 
-  return (
-    <div className="App">
-      <form className="search-form" onSubmit={getSearch}>
-        <input
-          ref={searchInput}
-          className="search-bar"
-          type="text"
-          value={search}
-          onChange={updateSearch}
-        />
-        <input className="search-button" type="submit" />
-      </form>
-      <div className="recipes">
-        {recipes.map((recipe) => (
-          <Recipe
-            title={recipe.recipe.label}
-            calories={recipe.recipe.calories}
-            image={recipe.recipe.image}
-            key={recipe.recipe.label}
-            ingredients={recipe.recipe.ingredientLines}
+  saveToStorage() {
+    localStorage.setItem("recipe_recs", JSON.stringify(this.state.recipe_recs));
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <form className="search-form" onSubmit={this.getSearch}>
+          <input
+            ref={this.searchInput}
+            className="search-bar"
+            type="text"
+            value={this.state.search}
+            onChange={this.updateSearch}
           />
-        ))}
+          <input className="search-button" type="submit" />
+        </form>
+        <div className="recipes">
+          {this.state.recipes.map((recipe) => (
+            <Recipe
+              title={recipe.recipe.label}
+              calories={recipe.recipe.calories}
+              image={recipe.recipe.image}
+              key={recipe.recipe.label}
+              ingredients={recipe.recipe.ingredientLines}
+            />
+          ))}
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default App;
