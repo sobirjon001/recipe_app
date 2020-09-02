eb60a195fa232a2f26babdc7e68444eaa4121140
sobirjon001 <sobirjon001@gmail.com>
2020-09-01 20:01:38 -0400

    new

--- src/App.js
+++ src/App.js
@@ -13,25 +13,55 @@ class App extends React.Component {
       APP_ID: "41456eaf",
       APP_KEY: "0c679c6c87d15f4a447a8c28afed63b2",
     };
-    this.loadStorage = this.loadStorage.bind(this);
-    this.getRecipes = this.getRecipes.bind(this);
-    this.updateSearch = this.updateSearch.bind(this);
-    this.getSearch = this.getSearch.bind(this);
-    this.saveToStorage = this.saveToStorage(this);
+    // this.loadStorage = this.loadStorage.bind(this);
   }
 
-  // load storage
   loadStorage() {
-    if (localStorage.getItem("recipe_recs") === null) {
-      localStorage.setItem("recipe_recs", JSON.stringify([]));
-    } else {
+    if (localStorage.getItem("recipe_recs")) {
       this.setState({
         recipe_recs: JSON.parse(localStorage.getItem("recipe_recs")),
       });
     }
   }
 
-  getRecipes() {
+  render() {
+    this.state.recipes.map((recipe) => {
+      console.log(recipe);
+    });
+    return (
+      <div className="App">
+        <div className="search-form">
+          <input
+            className="search-bar"
+            type="text"
+            onChange={(e) => {
+              this.setState({ search: e.target.value });
+            }}
+          />
+          <button
+            className="search-button"
+            onClick={() => {
+              this.setState({ query: this.state.search });
+              this.setState({ search: "" });
+              this.getRecipes();
+            }}
+          >
+            Submit
+          </button>
+          <button className="search-button" onClick={this.saveToStorage}>
+            Save
+          </button>
+        </div>
+        <div className="recipes">
+          {this.state.recipes.map((recipe) => (
+            <Recipe recipe={recipe} key={recipe.recipe.label} />
+          ))}
+        </div>
+      </div>
+    );
+  }
+
+  getRecipes = () => {
     if (
       this.state.recipe_recs.includes(
         (recipe_rec) => recipe_rec.title === this.state.query
@@ -46,71 +76,27 @@ class App extends React.Component {
       this.state.query === "   "
     ) {
       alert("Please wrigh valid recipe for cearch");
-      this.state.search("");
+      this.setState({ search: "" });
     } else {
       fetch(
         `https://api.edamam.com/search?q=${this.state.query}&app_id=${this.state.APP_ID}&app_key=${this.state.APP_KEY}`
       )
         .then((rec) => rec.json())
         .then((data) => {
-          console.log("happend 1");
-          console.log(data.hits);
-          this.setState({ recipes: [data.hits] });
-          console.log("happend 2");
-          console.log(data.hits);
+          this.setState({ recipes: data.hits });
           this.setState({
             recipe_recs: [
               ...this.state.recipe_recs,
-              { title: data.q, hits: [data.hits] },
+              { title: data.q, hits: data.hits },
             ],
           });
         });
-      this.saveToStorage();
     }
-  }
-  updateSearch(e) {
-    this.setState({ search: e.target.value });
-  }
+  };
 
-  getSearch(e) {
-    e.preventDafault();
-    console.log(e);
-    this.setState({ query: this.state.search });
-    this.setState({ search: "" });
-    this.getRecipes();
-  }
-
-  saveToStorage() {
+  saveToStorage = () => {
     localStorage.setItem("recipe_recs", JSON.stringify(this.state.recipe_recs));
-  }
-
-  render() {
-    return (
-      <div className="App">
-        <form className="search-form" onSubmit={this.getSearch}>
-          <input
-            ref={this.searchInput}
-            className="search-bar"
-            type="text"
-            value={this.state.search}
-            onChange={this.updateSearch}
-          />
-          <input className="search-button" type="submit" />
-        </form>
-        <div className="recipes">
-          {this.state.recipes.map((recipe) => (
-            <Recipe
-              title={recipe.recipe.label}
-              calories={recipe.recipe.calories}
-              image={recipe.recipe.image}
-              key={recipe.recipe.label}
-              ingredients={recipe.recipe.ingredientLines}
-            />
-          ))}
-        </div>
-      </div>
-    );
-  }
+  };
 }
 
 export default App;
--- src/Recipe.js
+++ src/Recipe.js
@@ -1,17 +1,19 @@
 import React from "react";
 import style from "./recipe.module.css";
 
-const Recipe = (title, calories, image, ingredients) => {
+const Recipe = (data) => {
+  console.log(data.recipe.recipe);
+  const ingredients = data.recipe.recipe.ingredientLines;
   return (
     <div className={style.recipe}>
-      <h1>{title}</h1>
+      <h1>{data.recipe.recipe.label}</h1>
       <ol>
         {ingredients.map((ingredient) => (
           <li>{ingredient}</li>
         ))}
       </ol>
-      <p>Calories: {calories}</p>
-      <img className={style.image} src={image} alt="" />
+      <p>Calories: {data.recipe.recipe.calories}</p>
+      <img className={style.image} src={data.recipe.recipe.image} alt="" />
     </div>
   );
 };
